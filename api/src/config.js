3bd85e7df5158b3677c79d73de61dea4c8cfe4a1
Nathan Rollins <ndrollin@asu.edu>
2024-10-18 11:35:12 -0700

    Update deployment variables

--- api/src/config.js
+++ api/src/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   database: {
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_DB,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
   },
   port: process.env.API_PORT || 3001,
   // if you're not using docker compose for local development, this will default to 8080
