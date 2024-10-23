78ed4a413753c9ccd48ba17867ff94071a86a916
Nathan Rollins <ndrollin@asu.edu>
2024-10-23 15:03:23 -0700

    Change readiness probe url

--- api/src/server.js
+++ api/src/server.js
@@ -55,11 +55,11 @@ sequelize
     //     .catch(next);
     // });
 
-    app.get('/healthz', function (req, res) {
+    app.get('/health', function (req, res) {
       // do app logic here to determine if app is truly healthy
       // you should return 200 if healthy, and anything else will fail
       // if you want, you should be able to restrict this to localhost (include ipv4 and ipv6)
-      res.send('Health check passed');
+      res.send(JSON.stringify({ "status": "Healthy" }));
     });
   })
   .catch((err) => {
