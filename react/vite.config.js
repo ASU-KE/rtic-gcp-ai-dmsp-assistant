b766ccd7426969b7fdcd4184f0ac63d7be546db3
Nathan Rollins <ndrollin@asu.edu>
2024-10-25 11:46:53 -0700

    Revert "Change Vitejs vars to system ENV variables"
    This reverts commit d3adfe93b270a7e7a5c04dbc3af625ee586b3e9a.

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,18 +1,12 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-    return {
-        define: {
-          'process.env.API_BACKEND_URL': JSON.stringify(env.API_BACKEND_URL),
-        },
-        plugins: [react()],
-        server: {
-          host: true,
-          port: 3000,
-          strictPort: true,
-        },
-    };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+  },
 });
