2a58f2ac41cc65d08e2007670e76e4bff3d43091
Nathan Rollins <ndrollin@asu.edu>
2024-10-29 11:19:04 -0700

    Fix routers reference

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
-import routers from './routes'
+import { routers } from './routes'
 
 const queryClient = new QueryClient();
 
