48b6e13e72caf8895dc81a7e5cdb8097d6bbe25a
aagar164 <aagar164@asu.edu>
2025-04-11 12:24:58 -0700

    feat(dmp): convert module to manual DI structure

--- api/src/dmp/routes.ts
+++ api/src/dmp/routes.ts
@@ -1,16 +1,25 @@
 import { Router } from 'express';
+import { DmpController } from './controllers/DmpController';
+
+import DmpService from './services/DmpService';
+import PdfService from './services/PdfService';
+import LlmService from './services/KE_LLM_Service';
 
 const router = Router();
-
-// Controller Imports
-import DmpController from './controllers/DmpController';
 
 // Middleware Imports
 // import isAuthenticatedMiddleware from '../common/middlewares/IsAuthenticatedMiddleware';
 // import SchemaValidationMiddleware from '../common/middlewares/SchemaValidationMiddleware';
 // import CheckPermissionMiddleware from '../common/middlewares/CheckPermissionMiddleware';
 
-router.post('/id', DmpController.getDmpReportById);
-router.post('/text', DmpController.getDmpReportByText);
+const dmpController = DmpController({
+  dmpService: DmpService,
+  pdfService: PdfService,
+  llmService: LlmService,
+});
+
+router.post('/id', dmpController.getDmpReportById);
+router.post('/text', dmpController.getDmpReportByText);
 
 export default router;
+
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,59 +1,79 @@
 import config from './config';
+import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import UserModel from './common/models/User';
 import Rollbar from 'rollbar';
-import app from './server';
+import { createApp } from './server';
 
 dotenv.config();
 const port = config.port;
-// include and initialize the rollbar library with your access token
+
+// Initialise Rollbar
 const rollbar = new Rollbar({
   accessToken: config.rollbarToken,
   captureUncaught: true,
   captureUnhandledRejections: true,
 });
 
-const server = app.listen(port, function () {
-  console.log('Server Listening on PORT:', config.port);
+const {
+  database: { host, database, user, password },
+} = config;
+
+const sequelize = new Sequelize(database, user, password, {
+  dialect: 'mysql',
+  host,
 });
 
-//
-// need this in docker container to properly exit since node doesn't handle SIGINT/SIGTERM
-// this also won't work on using npm start since:
-// https://github.com/npm/npm/issues/4603
-// https://github.com/npm/npm/pull/10868
-// https://github.com/RisingStack/kubernetes-graceful-shutdown-example/blob/master/src/index.js
-// if you want to use npm then start with `docker run --init` to help, but I still don't think it's
-// a graceful shutdown of node process
-//
+// Initialising the Model on sequelize
+UserModel.initialise(sequelize);
 
-// quit on ctrl-c when running docker in terminal
-process.on('SIGINT', function onSigint() {
-  rollbar.info(
-    'Got SIGINT (aka ctrl-c in docker). Graceful shutdown ',
-    new Date().toISOString()
-  );
-  shutdown();
-});
+sequelize
+  .sync()
+  .then(() => {
+    rollbar.info('Sequelize initialized!');
 
-// quit properly on docker stop
-process.on('SIGTERM', function onSigterm() {
-  rollbar.info(
-    'Got SIGTERM (docker container stop). Graceful shutdown ',
-    new Date().toISOString()
-  );
-  shutdown();
-});
+    // Create Express app by injecting dependencies
+    const app = createApp(rollbar, sequelize);
 
-// shut down server
-function shutdown() {
-  server.close(function onServerClosed(err) {
-    if (err) {
-      rollbar.error(err);
-      process.exit(1);
+    // Start listening
+    const server = app.listen(port, () => {
+      console.log(`Server listening on PORT: ${port}`);
+    });
+
+    // Graceful shutdown
+    function shutdown() {
+      server.close((err) => {
+        if (err) {
+          rollbar.error(err);
+          process.exit(1);
+        }
+        process.exit(0);
+      });
     }
-    process.exit(0);
+
+    //
+    // need this in docker container to properly exit since node doesn't handle SIGINT/SIGTERM
+    // this also won't work on using npm start since:
+    // https://github.com/npm/npm/issues/4603
+    // https://github.com/npm/npm/pull/10868
+    // https://github.com/RisingStack/kubernetes-graceful-shutdown-example/blob/master/src/index.js
+    // if you want to use npm then start with `docker run --init` to help, but I still don't think it's
+    // a graceful shutdown of node process
+    //
+
+    // Quit on Ctrl-C when running docker in terminal
+    process.on('SIGINT', () => {
+      rollbar.info('Got SIGINT (Ctrl-C). Graceful shutdown.', new Date().toISOString());
+      shutdown();
+    });
+
+    // Quit on Docker stop
+    process.on('SIGTERM', () => {
+      rollbar.info('Got SIGTERM (Docker stop). Graceful shutdown.', new Date().toISOString());
+      shutdown();
+    });
+  })
+  .catch((err: Error) => {
+    rollbar.error('Sequelize init error:', err);
+    process.exit(1);
   });
-}
-//
-// need above in docker container to properly exit
-//
