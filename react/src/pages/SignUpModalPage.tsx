f149b14ea47f9f1408e14da3488ab3c4b08517a3
aagar164 <aagar164@asu.edu>
2025-05-13 22:30:01 -0700

    add sign up

---
+++ react/src/pages/SignUpModalPage.tsx
@@ -0,0 +1,9 @@
+import { Modal } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { SignUp } from './SignUp';
+
+export const SignUpModalPage = () => {
+  const navigate = useNavigate();
+
+  return <SignUp show={true} onClose={() => navigate(-1)} />
+};
