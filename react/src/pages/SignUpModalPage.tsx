0abcc3b926ccad59e85d127f5fc1a6529d22e394
Nathan Rollins <ndrollin@asu.edu>
2025-05-28 15:36:40 -0400

    Drop redundant page from branch merge

--- react/src/pages/SignUpModalPage.tsx
+++
@@ -1,9 +0,0 @@
-import { Modal } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import { SignUp } from './Signup';
-
-export const SignUpModalPage = () => {
-  const navigate = useNavigate();
-
-  return <SignUp show={true} onClose={() => navigate(-1)} />;
-};
