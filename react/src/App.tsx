1d50b90b037f984abf650ec4334593da9c7b6c50
Nathan Rollins <ndrollin@asu.edu>
2025-07-14 18:29:31 -0400

    Refactor App.tsx for declarative Router and Auth

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,17 +1,39 @@
-import '@asu/unity-bootstrap-theme/dist/css/unity-bootstrap-theme.bundle.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { RouterProvider } from 'react-router-dom';
-import { routers } from './routes';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
-const queryClient = new QueryClient();
+import { useAuthContext } from './contexts/AuthContext';
+import { Layout } from './pages/Layout';
+import { HomePage } from './pages/HomePage';
+import { CasLoginPage } from './pages/CasLoginPage';
+import { SubmitDmpText } from './pages/SubmitDmpText';
+
+import '@asu/unity-bootstrap-theme/dist/css/unity-bootstrap-theme.bundle.css';
+
+const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+  const { user } = useAuthContext();
+  // Check if the user is authenticated
+  // If authenticated, render the children (protected content)
+  // If not authenticated, redirect to the login page
+  return user ? children : <Navigate to="/login" />;
+};
 
 const App = (): JSX.Element => {
+  const { user } = useAuthContext();
   return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={routers} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <CasLoginPage />} />
+        <Route
+          path="/submit-text"
+          element={
+            <ProtectedRoute>
+              {/* For testing auth: Protect just the Submit DMP page */}
+              <SubmitDmpText />
+            </ProtectedRoute>
+          }
+        />
+      </Route>
+    </Routes>
   );
 };
 
