ae7cb59b00c01e51f5101154cb9b08cbb5de4847
Nathan Rollins <ndrollin@asu.edu>
2025-07-14 15:11:24 -0400

    Disable Help button

--- react/src/pages/Layout.tsx
+++ react/src/pages/Layout.tsx
@@ -67,14 +67,14 @@ const header: TsHeaderProps = {
   isPartner: false,
   animateTitle: true,
   expandOnHover: true,
-  buttons:
-    [
-      {
-        "href": "/",
-        "text": "Request Help",
-        "color": "gold",
-      },
-    ]
+  // buttons:
+  //   [
+  //     {
+  //       "href": "/",
+  //       "text": "Request Help",
+  //       "color": "gold",
+  //     },
+  //   ]
 };
 
 const footer = {};
