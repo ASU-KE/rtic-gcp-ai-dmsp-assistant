cf629a92e0e41d220fe927b6f1604ed156dec536
Nathan Rollins <ndrollin@asu.edu>
2025-07-16 12:25:36 -0400

    Refactor index.ts to move server setup to server.js

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,112 +1,59 @@
-import Rollbar from 'rollbar';
-import http from 'http';
-import { WebSocketServer, WebSocket, RawData } from 'ws';
-
 import config from './config/app.config';
-import { AppDataSource } from './config/orm.config';
-import { configurePassport } from './config/passport.config';
-import { createApp } from './server';
 
-import { User } from './entities/User';
-import { UserService } from './modules/users/services/UserService';
-
-const port = config.port;
-
-// Initialise Rollbar
+// include and initialize the rollbar library with your access token
+import Rollbar from 'rollbar';
 const rollbar = new Rollbar({
   accessToken: config.rollbarToken,
   captureUncaught: true,
   captureUnhandledRejections: true,
 });
 
-AppDataSource.initialize()
-  .then(() => {
-    rollbar.info('TypeORM DataSource initialized!');
+import app from './server';
 
-    // Create a UserService instance
-    const userRepo = AppDataSource.getRepository(User);
-    const userService = new UserService(userRepo);
+const port = config.port;
 
-    // Configure Passport
-    configurePassport(userService);
+const server = app.listen(port, function () {
+  console.log('Server Listening on PORT:', port);
+});
 
-    // Create Express app by injecting dependencies
-    const app = createApp(rollbar, AppDataSource, userService);
+//
+// need this in docker container to properly exit since node doesn't handle SIGINT/SIGTERM
+// this also won't work on using npm start since:
+// https://github.com/npm/npm/issues/4603
+// https://github.com/npm/npm/pull/10868
+// https://github.com/RisingStack/kubernetes-graceful-shutdown-example/blob/master/src/index.js
+// if you want to use npm then start with `docker run --init` to help, but I still don't think it's
+// a graceful shutdown of node process
+//
 
-    // Add Passport middleware
-    // app.use(passport.initialize());
+// quit on ctrl-c when running docker in terminal
+process.on('SIGINT', function onSigint() {
+  rollbar.info(
+    'Got SIGINT (aka ctrl-c in docker). Graceful shutdown ',
+    new Date().toISOString()
+  );
+  shutdown();
+});
 
-    // eslint-disable-next-line @typescript-eslint/no-misused-promises
-    const server = http.createServer(app);
+// quit properly on docker stop
+process.on('SIGTERM', function onSigterm() {
+  rollbar.info(
+    'Got SIGTERM (docker container stop). Graceful shutdown ',
+    new Date().toISOString()
+  );
+  shutdown();
+});
 
-    const wss = new WebSocketServer({ server });
-
-    app.locals.wss = wss;
-
-    wss.on('connection', (ws: WebSocket) => {
-      ws.on('message', (data: RawData) => {
-        let message: string;
-
-        if (typeof data === 'string') {
-          message = data;
-        } else if (data instanceof Buffer) {
-          message = data.toString('utf-8');
-        } else if (data instanceof ArrayBuffer) {
-          message = Buffer.from(new Uint8Array(data)).toString('utf-8');
-        } else if (Array.isArray(data)) {
-          message = Buffer.concat(data).toString('utf-8');
-        } else {
-          message = '[Unrecognized message format]';
-        }
-        console.log('Received:', message);
-      });
-    });
-
-    // Start listening
-    server.listen(port, () => {
-      console.log(`Server listening on PORT: ${port}`);
-    });
-
-    // Graceful shutdown
-    function shutdown() {
-      server.close((err) => {
-        if (err) {
-          rollbar.error(err);
-          process.exit(1);
-        }
-        process.exit(0);
-      });
+// shut down server
+function shutdown() {
+  server.close(function onServerClosed(err) {
+    if (err) {
+      rollbar.error(err);
+      process.exit(1);
     }
-
-    //
-    // need this in docker container to properly exit since node doesn't handle SIGINT/SIGTERM
-    // this also won't work on using npm start since:
-    // https://github.com/npm/npm/issues/4603
-    // https://github.com/npm/npm/pull/10868
-    // https://github.com/RisingStack/kubernetes-graceful-shutdown-example/blob/master/src/index.js
-    // if you want to use npm then start with `docker run --init` to help, but I still don't think it's
-    // a graceful shutdown of node process
-    //
-
-    // Quit on Ctrl-C when running docker in terminal
-    process.on('SIGINT', () => {
-      rollbar.info(
-        'Got SIGINT (Ctrl-C). Graceful shutdown.',
-        new Date().toISOString()
-      );
-      shutdown();
-    });
-
-    // Quit on Docker stop
-    process.on('SIGTERM', () => {
-      rollbar.info(
-        'Got SIGTERM (Docker stop). Graceful shutdown.',
-        new Date().toISOString()
-      );
-      shutdown();
-    });
-  })
-  .catch((err: Error) => {
-    rollbar.error('TypeORM init error:', err);
-    process.exit(1);
+    process.exit(0);
   });
+}
+//
+// need above in docker container to properly exit
+//
