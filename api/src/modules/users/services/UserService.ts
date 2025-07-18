d8bb417a1e748ffa8bb40e6b170828407fe7e715
Nathan Rollins <ndrollin@asu.edu>
2025-07-18 19:17:57 -0400

    Add password helpers to userService

--- api/src/modules/users/services/UserService.ts
+++ api/src/modules/users/services/UserService.ts
@@ -1,6 +1,8 @@
 import { DataSource, Repository } from 'typeorm';
+import bcrypt from 'bcrypt';
 
 import { User } from '../../../entities/User';
+import config from '../../../config/app.config';
 
 export class UserService {
   private repo: Repository<User>;
@@ -27,5 +29,27 @@ export class UserService {
 
   deleteUser(query: Partial<User>) {
     return this.repo.delete(query);
+  }
+
+  async hashPassword(password: string): Promise<string> {
+    try {
+      return await bcrypt.hash(password, config.passwordSaltRounds);
+    } catch (err) {
+      console.error('Hashing failed:', err);
+      throw err;
+    }
+  }
+
+  async verifyPassword(
+    hashedPassword: string,
+    password: string
+  ): Promise<boolean> {
+    try {
+      // Verify the password against the hash
+      return await bcrypt.compare(password, hashedPassword);
+    } catch (err) {
+      console.error('Verification failed:', err);
+      throw err;
+    }
   }
 }
