edf5710774a53c4ea9803edcd32fee89b8f76cb8
Nathan Rollins <ndrollin@asu.edu>
2025-07-21 18:43:08 -0400

    Rename migrations

--- api/src/migrations/1753131697509-CreateUserTable.ts
+++ api/src/migrations/1753131697509-CreateUserTable.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddUser1753131697509 implements MigrationInterface {
-    name = 'AddUser1753131697509'
+export class CreateUserTable1753131697509 implements MigrationInterface {
+    name = 'CreateUserTable1753131697509'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`user\` (\`id\` int NOT NULL AUTO_INCREMENT, \`username\` varchar(255) NOT NULL, \`email\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, \`role\` varchar(255) NOT NULL DEFAULT 'user', \`firstName\` varchar(255) NOT NULL, \`lastName\` varchar(255) NOT NULL, UNIQUE INDEX \`IDX_78a916df40e02a9deb1c4b75ed\` (\`username\`), UNIQUE INDEX \`IDX_e12875dfb3b1d92d7d7c5377e2\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
--- api/src/migrations/1753133174224-CreateSessionTable.ts
+++ api/src/migrations/1753133174224-CreateSessionTable.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class AddSession1753133174224 implements MigrationInterface {
-    name = 'AddSession1753133174224'
+export class CreateSessionTable1753133174224 implements MigrationInterface {
+    name = 'CreateSessionTable1753133174224'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE \`session\` (\`expiredAt\` bigint NOT NULL, \`id\` varchar(255) NOT NULL, \`json\` text NOT NULL, \`destroyedAt\` datetime(6) NULL, INDEX \`IDX_28c5d1d16da7908c97c9bc2f74\` (\`expiredAt\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
