3f44fb5a0488beca7bd9d77fdda9a93f7a4ac8c9
Nathan Rollins <ndrollin@asu.edu>
2025-07-21 13:22:07 -0400

    Update auth routes

--- api/src/routes/auth.routes.ts
+++ api/src/routes/auth.routes.ts
@@ -1,21 +1,27 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import passport from 'passport';
 
-import SchemaValidationMiddleware from '../middlewares/SchemaValidationMiddleware';
+import SchemaValidationMiddleware from '../middlewares/schema-validation.middleware';
 import loginPayload from '../modules/auth/schemas/loginPayload';
 
 const authRoutes = () => {
   const router: Router = Router();
 
-  router.post('/login', [
-    SchemaValidationMiddleware.verify(loginPayload),
-    passport.authenticate('local', {
-      successRedirect: '/',
-      failureRedirect: '/login',
-    }),
-  ]);
+  router.post(
+    '/login',
+    [
+      SchemaValidationMiddleware.verify(loginPayload),
+      passport.authenticate('local', {
+        failureRedirect: '/login',
+        failureMessage: true,
+      }),
+    ],
+    function (req: Request, res: Response) {
+      res.redirect('/');
+    }
+  );
 
-  router.get('/logout', (req, res) => {
+  router.get('/logout', (req: Request, res: Response) => {
     req.logout((err) => {
       if (err) {
         return res.status(500).json({ message: 'Logout failed' });
