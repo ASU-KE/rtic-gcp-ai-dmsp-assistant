6ada0cec21ceb914b4735dbbf320e36b4324bba2
Nathan Rollins <ndrollin@asu.edu>
2025-07-21 15:34:44 -0400

    Refactor checkPermission middleware

--- api/src/middlewares/check-permission.middleware.ts
+++ api/src/middlewares/check-permission.middleware.ts
@@ -1,23 +1,18 @@
-import { RequestHandler } from 'express';
 import { Request, Response, NextFunction } from 'express';
 import { Role } from '../config/app.config';
 
-export function CheckPermissionMiddleware() {
-  const has = (role: Role): RequestHandler => {
-    return (req: Request, res: Response, next: NextFunction) => {
-      const { user } = req;
+export const checkPermission = (role: Role) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { user } = req;
 
-      if (!user || user.role !== role) {
-        res.status(403).json({
-          status: false,
-          error: `Unauthorized`,
-        });
-        return;
-      }
+    if (!user || user.role !== role) {
+      res.status(403).json({
+        status: false,
+        error: `Unauthorized`,
+      });
+      return;
+    }
 
-      next();
-    };
+    next();
   };
-
-  return { has };
-}
+};
