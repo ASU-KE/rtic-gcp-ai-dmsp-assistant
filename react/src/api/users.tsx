4ce4a60d6b481dac5296ae7fceceeb3b605fd446
Nathan Rollins <ndrollin@asu.edu>
2025-07-25 15:51:59 -0400

    Add api query functions

---
+++ react/src/api/users.tsx
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { User } from '../types';
+
+export async function getCurrentUser(): Promise<User> {
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_BACKEND_PROTOCOL}://${import.meta.env.VITE_BACKEND_DOMAIN}:${import.meta.env.VITE_BACKEND_PORT}/user/current`,
+      {
+        withCredentials: true,
+      }
+    );
+
+    // check for unauthorized response status
+    if (response.status === 401) {
+      throw new Error('Unauthorized: Please log in');
+    }
+
+    // check if response status is not OK
+    if (response.status !== 200 || !response.data || !response.data.user) {
+      throw new Error(`Error fetching user: ${response.statusText}`);
+    }
+
+    const { user } = response.data;
+    console.log('Fetched user:', user);
+    return user;
+
+  } catch (err: any) {
+    console.error('API query error:', err);
+    throw new Error(err.response?.data?.error?.message || 'General API query error');
+  }
+}
