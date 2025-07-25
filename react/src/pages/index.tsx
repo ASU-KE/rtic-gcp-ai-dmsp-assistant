f7e47c16841780c5616261c39466b67454acbc98
Nathan Rollins <ndrollin@asu.edu>
2025-07-25 14:59:58 -0400

    Add base pages index

---
+++ react/src/pages/index.tsx
@@ -0,0 +1,20 @@
+import { LoginPage, LogoutPage, CasLoginPage } from './auth';
+import { SubmitDmpText, SubmitDmpId } from './dmsp';
+import { CreateUserPage, DeleteUserPage, ListUsersPage, UpdateUserPage } from './user';
+
+import { HomePage } from './HomePage';
+import { Layout } from './Layout';
+
+export {
+  HomePage,
+  Layout,
+  LoginPage,
+  LogoutPage,
+  CasLoginPage,
+  SubmitDmpText,
+  SubmitDmpId,
+  CreateUserPage,
+  DeleteUserPage,
+  ListUsersPage,
+  UpdateUserPage,
+};
