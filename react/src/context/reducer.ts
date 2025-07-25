f1c7e0f9e202b2c1129e42c38e17d95fdef9a4cc
Nathan Rollins <ndrollin@asu.edu>
2025-07-25 14:37:15 -0400

    Add Auth context state reducer and provider

---
+++ react/src/context/reducer.ts
@@ -0,0 +1,21 @@
+import { AuthAction, AuthState } from "../types";
+
+export const AuthReducer = (state: AuthState, action: AuthAction): AuthState => {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload.user,
+      };
+
+    case "LOGOUT":
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
+    default:
+      return state;
+  }
+};
---
+++ react/src/types/context.d.ts
@@ -0,0 +1,18 @@
+import { Dispatch, ReactNode } from 'react';
+import { User } from './user';
+
+export interface AuthState {
+  isAuthenticated: boolean;
+  user: null | User;
+}
+
+export interface AuthContextProps {
+  state: AuthState;
+  dispatch: Dispatch<AuthAction>;
+}
+
+export interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export type AuthAction = { type: 'LOGIN'; payload: { user: User; } } | { type: 'LOGOUT' };
