c7123457671887f4e5b77df9719321b75fc68379
Nathan Rollins <ndrollin@asu.edu>
2025-07-25 14:38:01 -0400

    Wrap App with AuthProvider

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-// import { useAuthContext } from './contexts/AuthContext';
+import { AuthProvider } from './context/provider';
+
 import { Layout } from './pages/Layout';
 import { HomePage } from './pages/HomePage';
 import { SubmitDmpText } from './pages/SubmitDmpText';
@@ -28,25 +29,27 @@ const queryClient = new QueryClient();
 const App = (): JSX.Element => {
   // const { user } = useAuthContext();
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/submit-text" element={<SubmitDmpText />} />
-            {import.meta.env.VITE_FRONTEND_ENABLE_DMP_ID && <Route path="/submit-id" element={<SubmitDmpId />} />}
+    <AuthProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/submit-text" element={<SubmitDmpText />} />
+              {import.meta.env.VITE_FRONTEND_ENABLE_DMP_ID && <Route path="/submit-id" element={<SubmitDmpId />} />}
 
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/logout" element={<LogoutPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/logout" element={<LogoutPage />} />
 
-            <Route path="/user/create" element={<CreateUserPage />} />
-            <Route path="/user/delete" element={<DeleteUserPage />} />
-            <Route path="/user/update" element={<UpdateUserPage />} />
-            <Route path="/user/all" element={<ListUsersPage />} />
-          </Route>
-        </Routes>
-      </QueryClientProvider>
-    </BrowserRouter>
+              <Route path="/user/create" element={<CreateUserPage />} />
+              <Route path="/user/delete" element={<DeleteUserPage />} />
+              <Route path="/user/update" element={<UpdateUserPage />} />
+              <Route path="/user/all" element={<ListUsersPage />} />
+            </Route>
+          </Routes>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </AuthProvider>
   );
 };
 
