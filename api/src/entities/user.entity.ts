d674489b25c34423ab02f32c627deff405e213b4
Nathan Rollins <ndrollin@asu.edu>
2025-08-01 17:15:28 -0400

    Add DTO decorators to User

--- api/src/entities/user.entity.ts
+++ api/src/entities/user.entity.ts
@@ -1,26 +1,34 @@
+import { Exclude, Expose } from 'class-transformer';
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 import config from '../config/app.config';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
+  @Expose()
   id!: number;
 
   @Column({ unique: true })
+  @Expose()
   username!: string;
 
   @Column({ unique: true })
+  @Expose()
   email!: string;
 
   @Column()
+  @Exclude()
   password!: string;
 
   @Column({ default: config.roles.USER })
+  @Expose()
   role!: string;
 
   @Column()
+  @Expose()
   firstName!: string;
 
   @Column()
+  @Expose()
   lastName!: string;
 }
