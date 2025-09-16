02af2ba70002d5e81a83ae4d74c2520d0be8079d
aagar164 <aagar164@asu.edu>
2025-09-15 18:09:25 -0700

    fix(api): refactor prompt logic to diff file

--- api/src/modules/dmp/services/KE_LLM_Service.ts
+++ api/src/modules/dmp/services/KE_LLM_Service.ts
@@ -2,6 +2,7 @@ import config from '../../../config/app.config';
 import { Rubric } from '../../../entities/rubric.entity';
 import { AppDataSource } from '../../../config/data-source.config';
 import { WebSocketServer, WebSocket } from 'ws';
+import promptConfig from '../../../config/llmPrompt.config';
 
 interface LlmResponse {
   response: string;
@@ -50,7 +51,10 @@ export default {
       },
     } = config;
 
-    const prompt = `You are a research administrator evaluating data management plans. For each section of performance criteria, determine whether the plan is complete/detailed, addressed issue but incomplete, or did not address. Skip sections that are complete/detailed. If a section of criteria is not complete/detailed, quote the relevant text from the plan, and provide a list of recommended improvements. Format each section with a section header. Use the following rubric to assess the data management plan: ${rubric.rubricText}`;
+    const prompt = promptConfig.prompt.replace(
+      '{{RUBRIC_TEXT}}',
+      rubric.rubricText
+    );
 
     const system_prompt = sourceType === 'file' ? prompt : sourceValue;
 
