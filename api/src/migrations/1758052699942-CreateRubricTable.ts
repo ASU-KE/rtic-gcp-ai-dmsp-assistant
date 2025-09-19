11034d418300a7e44e760e4f0b9021a29f344474
Nathan Rollins <ndrollin@asu.edu>
2025-09-19 13:50:27 -0400

    fix(api): fix db migration

--- api/src/migrations/1758052699942-CreateRubricTable.ts
+++ api/src/migrations/1758052699942-CreateRubricTable.ts
@@ -5,7 +5,7 @@ export class CreateRubricTable1758052699942 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE \`rubric\` (\`id\` int NOT NULL AUTO_INCREMENT, \`agency\` enum ('NSF', 'DOE', 'DOD', 'NIH', 'NASA', 'NOAA', 'USDA', 'USGS') NOT NULL, \`rubricText\` text NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), UNIQUE INDEX \`IDX_9557bd1d6a6342ee92eed3eb8a\` (\`agency\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
+      `CREATE TABLE \`rubric\` (\`id\` int NOT NULL AUTO_INCREMENT, \`agency\` varchar(255) NOT NULL, \`rubricText\` text NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), INDEX \`IDX_9557bd1d6a6342ee92eed3eb8a\` (\`agency\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`
     );
   }
 
